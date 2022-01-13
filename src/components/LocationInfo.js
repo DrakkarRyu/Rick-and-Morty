5493e9384fd757098dd502723c8b59d9b9213cda
DrakkarRyu <javisodst-metal@outlook.com>
2022-01-13 17:00:58 -0600

    first commit

---
+++ src/components/LocationInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LocationInfo = ({type,population,dimension}) => {
+    return (
+        <ul>
+            <li>Type:{type}</li>
+            <li>Population:{population}</li>
+            <li>Dimension:{dimension}</li>
+        </ul>
+    );
+};
+
+export default LocationInfo;
---
+++ src/components/ResidentsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ResidentInfo from "./ResidentInfo"
+
+const ResidentsList = ({ location }) => {
+    return (
+        <div className="residentsListContainer">
+            {location.residents.slice(0,10).map((resident) => (
+            <div key={resident}>
+            <ResidentInfo residentURL={resident} />
+            </div>
+        ))}
+        </div>
+    );
+};
+
+export default ResidentsList;  
